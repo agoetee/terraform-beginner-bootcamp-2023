d8181e6447937a0a2e8e7b59266f17c4fd5970d9
Charles Agoe <agoetee@gmail.com>
2023-09-23 11:51:05 +0000

    #17 edit the set alias script to find if any exists

--- bin/set_tf_alias.sh
+++ bin/set_tf_alias.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Define the alias
-alias tf='terraform'
+alias_name='tf'
+alias_command='terraform'
 
 # Check if .bash_profile exists
 if [ -f ~/.bash_profile ]; then
-  # Append the alias to .bash_profile
-  echo 'alias tf="terraform"' >> ~/.bash_profile
-  # Source the updated .bash_profile to make the alias immediately available
-  source ~/.bash_profile
-  echo "Alias 'tf' for 'terraform' has been added to .bash_profile."
+  # Check if the alias already exists in .bash_profile
+  if grep -q "alias $alias_name=" ~/.bash_profile; then
+    echo "Alias '$alias_name' for '$alias_command' already exists in .bash_profile."
+  else
+    # Append the alias to .bash_profile
+    echo "alias $alias_name='$alias_command'" >> ~/.bash_profile
+    # Source the updated .bash_profile to make the alias immediately available
+    source ~/.bash_profile
+    echo "Alias '$alias_name' for '$alias_command' has been added to .bash_profile."
+  fi
 else
   echo "Error: .bash_profile does not exist in your home directory."
   exit 1
