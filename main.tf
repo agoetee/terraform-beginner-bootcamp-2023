214c2695f0774859c858d39bb9632fe8fc756e20
Charles Agoe <agoetee@gmail.com>
2023-10-01 10:50:06 +0000

    #25 refactor S3 bucket into new terrahouse_aws module

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
-# Modules - Random
-
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "terrahouse_website" {
-  bucket = var.bucket_name
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-
-    tags = {
-    UserUuid    = var.user_uuid
-  }
-
+terraform {
+#  cloud {
+#    organization = "agoetee"
+#    workspaces {
+#      name = "terra-house-1"
+#    }
+#  }
 }
 
-
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  bucket_name = var.bucket_name
+  user_uuid = var.user_uuid
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "bucket_name" {
-  value = aws_s3_bucket.terrahouse_website.bucket
+  description = "bucket name for our website hosting"
+  value = module.terrahouse_aws.bucket_name
 }
