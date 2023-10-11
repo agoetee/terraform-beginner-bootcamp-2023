c07b8a2b9509735e879c6bc6595f011b78481ca1
Charles Agoe <agoetee@gmail.com>
2023-10-11 08:24:14 +0000

    #41 run the server CRUD actions actually

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -169,7 +169,6 @@ class TerraTownsMockServer < Sinatra::Base
     # Validate payload data
     name = payload["name"]
     description = payload["description"]
-    domain_name = payload["domain_name"]
     content_version = payload["content_version"]
 
     unless params[:uuid] == $home[:uuid]
@@ -178,9 +177,9 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
 
     unless home.valid?
