ca19b7e2612f63eda0d89b9c866fcf4013c4f961
Charles Agoe <96971156+agoetee@users.noreply.github.com>
2023-10-11 18:17:20 +0100

    44 terratowns provider (#46)
    * #44 work-in-progress setting up skeleton for custom tf provider
    
    * #44 implement terraform provider block

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "bucket name for our website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
-
-output "s3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
-
-output "cloudfront_url" {
-  description = "The cloudfront distribution Domain name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+#output "bucket_name" {
+#  description = "bucket name for our website hosting"
+#  value = module.terrahouse_aws.bucket_name
+#}
+#
+#output "s3_website_endpoint" {
+#  description = "S3 static website hosting endpoint"
+#  value = module.terrahouse_aws.website_endpoint
+#}
+#
+#output "cloudfront_url" {
+#  description = "The cloudfront distribution Domain name"
+#  value = module.terrahouse_aws.cloudfront_url
+#}
