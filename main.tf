388a6ed609ac4d4ea9ac7353e0e69544d97a8ae6
Charles Agoe <96971156+agoetee@users.noreply.github.com>
2023-10-12 17:28:49 +0100

    44 terratowns provider (#48)
    * #44 work-in-progress setting up skeleton for custom tf provider
    
    * #44 implement terraform provider block
    
    * #44 set up skeleton for resource
    
    * #44 finish implementing terraform provider

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 
@@ -30,3 +30,17 @@ provider "terratowns" {
 #  content_version = var.content_version
 #  assets_path = var.assets_path
 #}
+
+resource "terratowns_home" "the_ampe" {
+  name = "the game ampe"
+  description = <<DESCRIPTION
+  Ampe is a game mainly played by ladies.
+The basic features involve clapping the hands in some unison and jumping.
+Most of the time, rythmic singing that goes with the clapping and jumping.
+DESCRIPTION
+
+  #domain_name = "module.terrahouse_aws.cloudfront_url"
+  domain_name = "21dewe.cloudfront.net"
+  town = "missingo"
+  content_version = 1
+}
