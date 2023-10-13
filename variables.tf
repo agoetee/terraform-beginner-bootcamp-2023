54ea27f32e9040876b8ce94a966ec6067e9c752b
Charles Agoe <agoetee@gmail.com>
2023-10-13 01:25:19 +0000

    #49 test terrahome on terratown cloud

--- variables.tf
+++ variables.tf
@@ -1,4 +1,11 @@
-variable "user_uuid" {
+variable "terratowns_access_token" {
+  type = string
+}
+variable "teacherseat_user_uuid" {
+  type = string
+}
+
+variable "terratowns_endpoint" {
   type = string
 }
 
